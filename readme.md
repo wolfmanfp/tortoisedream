d452c009dbed70750f4cac1390ab198baea2b9fd
Farkas Péter <farkas.peter1995@gmail.com>
2016-05-09 22:30:57 +0200

    Added tortoise image, repaint method

---
+++ readme.md
@@ -0,0 +1,5 @@
+# Tortoise Dream
+
+## Assets
+
+Turtle icon made by [Freepik](http://www.freepik.com) from [www.flaticon.com](http://www.flaticon.com) is used under [Creative Commons BY 3.0 license](http://creativecommons.org/licenses/by/3.0/) (recolored from original).
--- tortoise.js
+++ tortoise.js
@@ -1,43 +1,47 @@
 var tortoise;
+var ctx;
 
-function Tortoise(X, Y, w, h, c){
-	var x = X;
-	var y = Y;
-	var angle=0;
-	var distance = 0;
-	var width = w;
-	var height = h;
-	var ctx = c;
-	
-	this.go = function(dist){
-		distance = dist;
-		x+= Math.sin(angle)*distance;
-		y+= Math.cos(angle)*distance;
-		this.draw();
-	}
-	
-	this.turn = function(deg){
-		angle+= deg* (Math.PI / 180);
-		this.draw();
-	}
-	
-	this.draw = function(){
-		ctx.save();
-		ctx.translate(x,y);
-		ctx.rotate(-angle);
-		ctx.fillRect(-10,- 10, w, h);
-		ctx.restore();
-	}
-	
-	
+function Tortoise(X, Y) {
+    var x = X;
+    var y = Y;
+    var angle = 0;
+
+    var img = new Image();
+    img.src = "tortoise.png";
+    var width = 26;
+    var height = 34;
+
+    this.go = function (dist) {
+        x -= Math.sin(angle) * dist;
+        y -= Math.cos(angle) * dist;
+    }
+
+    this.turn = function (deg) {
+        angle -= deg * (Math.PI / 180);
+    }
+
+    this.draw = function () {
+        ctx.save();
+        ctx.translate(x, y);
+        ctx.rotate(-angle);
+        ctx.drawImage(img, 0 - (width / 2), 0 - (height / 2));
+        ctx.restore();
+    }
+
+
 }
 
-function init(ctx, x, y){
-	tortoise = new Tortoise(x, y, 20, 20, ctx);
+function init(x, y) {
+    tortoise = new Tortoise(x, y);
+    setInterval(repaint, 33);
+}
+
+function repaint() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    tortoise.draw();
 }
 
 window.onload = function () {
-	var canvas = document.getElementById("canvas");
-	var ctx = canvas.getContext("2d");
-	init(ctx, canvas.width/2, canvas.height/2);
+    ctx = document.getElementById("canvas").getContext("2d");
+    init(canvas.width / 2, canvas.height / 2);
 }
