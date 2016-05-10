1ec77f9ac5020740e49c5b04d7cecc2b056c9a72
Farkas János <jancsiij@gmail.com>
2016-05-10 16:31:21 +0200

    animate

--- tortoise.js
+++ tortoise.js
@@ -1,10 +1,15 @@
 var tortoise;
 var ctx;
+var ctx2;
 
 function Tortoise(X, Y) {
     var x = X;
     var y = Y;
+	var degree =0;
+	var degreeStep =0;
     var angle = 0;
+	var distance = 0;
+	var step = 1;
 
     var img = new Image();
     img.src = "tortoise.png";
@@ -12,23 +17,42 @@ function Tortoise(X, Y) {
     var height = 34;
 
     this.go = function (dist) {
-        x -= Math.sin(angle) * dist;
-        y -= Math.cos(angle) * dist;
+		distance = dist;
     }
 
     this.turn = function (deg) {
-        angle -= deg * (Math.PI / 180);
+		degree = deg;
+		degreeStep = degree/5;
     }
 
     this.draw = function () {
-        ctx.save();
+		
+		move();
+		rotate();
+		
+		ctx.save();
         ctx.translate(x, y);
         ctx.rotate(-angle);
         ctx.drawImage(img, 0 - (width / 2), 0 - (height / 2));
         ctx.restore();
+		
+		ctx2.fillRect(x, y , 1, 1 );
     }
 
-
+	function rotate(){
+		if(degree !=0){
+			angle -= degreeStep * (Math.PI / 180);
+			degree -=degreeStep;
+		}
+	}
+	
+	function move(){
+		if(distance != 0){
+			x -= Math.sin(angle) * step;
+			y -= Math.cos(angle) * step;
+			distance -=step;
+		}
+	}
 }
 
 function init(x, y) {
@@ -43,5 +67,6 @@ function repaint() {
 
 window.onload = function () {
     ctx = document.getElementById("canvas").getContext("2d");
+    ctx2 = document.getElementById("canvas2").getContext("2d");
     init(canvas.width / 2, canvas.height / 2);
 }
